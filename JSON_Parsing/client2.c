64cef9c89cbf8f728a677e110740bd200a94c4ec
Mbapat2211 <22mihirbapat11@gmail.com>
2022-12-02 23:24:28 +0530

    JSON Parsing

---
+++ JSON_Parsing/client2.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include"handler.h"
+#include<stdlib.h>
+
+int main()
+{
+    int socket_desc;
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0); // initialize socket
+    
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    
+    struct sockaddr_in addrport; // defined by c socket api
+    addrport.sin_family = AF_INET;
+    addrport.sin_port = htons(8880); // port number
+    addrport.sin_addr.s_addr = inet_addr("127.0.0.1");
+    
+    int c_status = connect(socket_desc, (struct sockaddr *)&addrport, sizeof(addrport)); // connect to the server
+    
+    if(c_status < 0)
+    {
+        perror("connect failed");
+        return 1;
+    }
+
+    char msg[MAXBUF];
+    
+    if(recv(socket_desc, msg, MAXBUF, 0) < 0)
+    {
+        puts("recv failed");
+        return 1;
+    }
+
+    puts(msg);
+
+    char *message = "Hello from Seller\0";
+
+    if(send(socket_desc , message , strlen(message) , 0) < 0)
+	{
+		puts("Send failed");
+		return 1;
+	}
+    
+    close(socket_desc);
+    return 0;
+}
